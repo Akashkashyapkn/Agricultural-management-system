1d3b62dc1fd64dda1aa9a62b053489d774ac9b36
AKASH KASHYAP KN <159678949+Akashkashyapkn@users.noreply.github.com>
2025-09-29 14:47:38 +0530

    Update db.php

--- db.php
+++ db.php
@@ -12,3 +12,16 @@
     }
 
 ?>
+
+
+<?php
+require 'vendor/autoload.php'; // If using Composer
+
+$uri = "mongodb://agroworld:qu5V9fEwOEgzYYRwcThgogcfOOhVYhoaFFrnlGrVnfT1Ph7cLxzWMNp9qjEWiZOBtZMbosIXttFpACDbf3alKg==@agroworld.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb";
+$client = new MongoDB\Client($uri);
+
+$database = $client->selectDatabase('agrodb');
+$collection = $database->selectCollection('agroworld001');
+
+// Example: Insert a document
+$collection->insertOne(['name' => 'Akash', 'location' => 'Bengaluru']);
